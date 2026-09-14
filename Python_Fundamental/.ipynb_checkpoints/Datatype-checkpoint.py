58e5c0dfad811c06196d4b4570ad09371a4730bc
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-09-14 13:21:17 +0545

    Dictionaries and Sets

---
+++ Python_Fundamental/.ipynb_checkpoints/Datatype-checkpoint.py
@@ -0,0 +1,15 @@
+# Integer (int) -> Whole numbers
+students = 156
+print(type(students))  # Tells you the type: <class 'int'>
+
+# Float (float) -> Numbers with decimals
+accuracy = 0.98
+print(type(accuracy))  # Tells you the type: <class 'float'>
+
+# String (str) -> Text (always put inside quotes "")
+topic = "Machine Learning"
+print(type(topic))     # Tells you the type: <class 'str'>
+
+# Boolean (bool) -> True or False (capital T and F)
+is_easy = False
+print(type(is_easy))   # Tells you the type: <class 'bool'>
